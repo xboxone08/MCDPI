17d67a3721df0b142667969fb79d06701f1f3b17
Xbox One <sondad2008@gmail.com>
2022-07-29 15:36:18 -0400

    Enhanced JSON config and fixed code
    Also added new code.

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import mcpi.minecraft as minecraft
+from json import load, dump
+from time import sleep
+
+with open("config.jsonc", 'r') as conf_file:
+    config = load(conf_file)
+heroes: dict = config["heroes"]
+settings: dict = config["settings"]
+hero: str = settings["default_hero"]
+camera_angle: str = settings["camera_angle"]
+fps: int = 1/settings["clock"]
+
+assert(hero in heroes)
+assert(camera_angle == "flat" or camera_angle == "angled")
 
 game = minecraft.Minecraft.create()
 
-player = game.getPlayerEntityIds()[0]
+player_id = game.getPlayerEntityIds()[0]
 
 # Set up for Minecraft Dungeons
-game.camera.setFollow(player)
+if camera_angle == "flat":
+    game.camera.setFollow(player_id)
+
 game.setting("world_immutable", True)
 
 while True:
-    
+    if camera_angle == "angled":
+        game.camera.setPos(game.player.getPos().x - 10,
+                           game.player.getPos().y + 10,
+                           game.player.getPos().z - 10)
+    sleep(fps)

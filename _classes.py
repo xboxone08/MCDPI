405d42f654b9f844af172ec41d9c92e70bb21b30
Xbox One <sondad2008@gmail.com>
2022-08-21 15:52:54 -0400

    Added more methods to _classes.py.

--- _classes.py
+++ _classes.py
@@ -1,10 +1,41 @@
-import json
+from __future__ import annotations
+from typing import List
+from json import load, dump
+
+team_lives: int = 4
+night: bool = False
 
 
 class Player:
-    def __init__(self, hero: str):
-        self.hero = hero
-        self.inventory = set()
+    players: List[Player] = []
 
-    def kill(self):
-        pass
+    def __init__(self, hero: str) -> None:
+        self.hero = hero
+        self.inventory: set = set()
+        self.dead: bool = False
+        with open('config.json', 'r') as file:
+            config = load(file)['heroes'][hero]
+        self.levels = config['level']
+
+
+    def die(self) -> None:
+        dead_players: List[Player] = []
+        players = Player.players
+        for player in players:
+            if player.dead:
+                dead_players.append(player)
+        if len(dead_players) == len(players):
+            for player in players:
+                player.dead = False
+            global team_lives
+            team_lives -= 1
+    
+    def level_up(self) -> None:
+        self.level += 1
+        self.enchantment_points += 1
+
+    @classmethod
+    def create(cls, hero: str) -> Player:
+        player = cls(hero)
+        cls.players.append(player)
+        return player
--- main.py
+++ main.py
@@ -2,24 +2,28 @@ import mcpi.minecraft as minecraft
 from json import load, dump
 from time import sleep
 
-with open("config.json", 'r') as conf_file:
-    config = load(conf_file)
+__all__ = tuple()
+
+with open("config.json", 'r') as file:
+    config = load(file)
 heroes: dict = config["heroes"]
 settings: dict = config["settings"]
 hero: str = settings["default_hero"]
 camera_angle: str = settings["camera_angle"]
 fps: int = 1/settings["fps"]
 
-assert(hero in heroes)
-assert(camera_angle == "flat" or camera_angle == "angled")
+if hero not in heroes:
+    raise ValueError("Configured `hero` has not been defined in `heroes`.")
+if camera_angle != "flat" and camera_angle != "angled":
+    raise ValueError('`camera_angle` must be either "flat" or "angled".')
 
 game = minecraft.Minecraft.create()
 
-player_id = game.getPlayerEntityIds()[0]
+players = game.getPlayerEntityIds()
 
 # Set up for Minecraft Dungeons
 if camera_angle == "flat":
-    game.camera.setFollow(player_id)
+    game.camera.setFollow(players[0])
 
 game.setting("world_immutable", True)
 
